54ce66681926c697c8f6377cfa8e5aa7955175b1
dinda fristiani <dindafristiani@gmail.com>
2022-06-11 19:17:46 +0700

    minnor

--- auth.js
+++ auth.js
@@ -7,17 +7,17 @@ admin.initializeApp({
 
 //create new user with auth
 //app.post('/signup', async (req,res) => {
-	//const user = {
-		//email : req.body.email,
-		//password : req.body.password
-	//}
-	//const userRespone = await admin.auth().createUser({
-		//email: user.email,
-		//password: user.password,
-		//emailVerified: false,
-		//disabled : false
-	//});
-	//res.json(userRespone);
+//	const user = {
+//		email : req.body.email,
+//		password : req.body.password
+//	}
+//	const userRespone = await admin.auth().createUser({
+//		email: user.email,
+//		password: user.password,
+//		emailVerified: false,
+//		disabled : false
+//	});
+//	res.json(userRespone);
 
 //})
 
